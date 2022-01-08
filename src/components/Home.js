33118d36075c5aaa8a62701e7942f2aa4e0c31c6
El hamine Elmehdi <elmehdi99elhamine@gmail.com>
2022-01-08 10:09:43 +0100

    ADD:Sections for the TESLA

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,52 @@ const Home = () => {
     return (
         <div>
             <Container>
-                <Section/>
+            <Section
+                title="Model 3" 
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-3.jpg"
+                leftbuttonText="Custom order"
+                rightbuttonText="Existing inventory"/>
+                
+
+                 <Section
+                title="Model Y" 
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-y.jpg"
+                leftbuttonText="Custom order"
+                rightbuttonText="Existing inventory"/>
+                <Section
+                title="Model S" 
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-s.jpg"
+                leftbuttonText="Custom order"
+                rightbuttonText="Existing inventory"/>
+
+                <Section
+                title="Model X" 
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-x.jpg"
+                leftbuttonText="Custom order"
+                rightbuttonText="Existing inventory"/>
+                
+                 <Section
+                title="Solar Panels" 
+                description="Lowest Cost Solar Panels in America"
+                backgroundImg="solar-panel.jpg"
+                leftbuttonText="Order Now"
+                rightbuttonText="Learn more"/>
+                <Section
+                title="Solar Roof" 
+                description="Produce Clean Energy From Your Roof"
+                backgroundImg="solar-roof.jpg"
+                leftbuttonText="Order Now"
+                rightbuttonText="Learn more"/>
+                 <Section
+                title="Accesories" 
+                
+                backgroundImg="accessories.jpg"
+                leftbuttonText="Shop Now"
+                rightbuttonText=""/>
             </Container>
         </div>
     )
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,20 +1,84 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Section = () => {
+const Section = ({title,description,leftbuttonText,rightbuttonText, backgroundImg}) => {
     return (
-        <Wrapper>
-            section
+        <Wrapper bgImage={backgroundImg}>
+            <ItemText>
+                <h1>{title}</h1>
+                <p>{description}</p>
+              
+            </ItemText>
+            <Buttons>
+            <ButtonGroupe>
+                <LeftButton>{leftbuttonText}</LeftButton>
+                {rightbuttonText &&  <RightButton>{rightbuttonText}</RightButton>}
+               
+            </ButtonGroupe>
+            <i style={{color:'white',animation:"animateDown infinite 1.5s",overflowX:"hidden",}}  class="far fa-angle-double-down fa-2x"></i>
+            </Buttons>
+           
         </Wrapper>
     )
 }
-
+//wrapper component
 const Wrapper=styled.div`
 width:100vw;
 height:100vh;
 background-size:cover;
 background-position:center;
 background-repeat:no-reapeat;
-background-image:url('/images/model-s.jpg');
+background-image:${props=>`url("/images/${props.bgImage}")`};
+display:flex;
+flex-direction:column;
+justify-content:space-between;
+align-items:center;
 `;
+//Item text component
+
+const ItemText=styled.div`
+padding-top:15vh;
+text-align:center;`;
+//Button groupe component
+const ButtonGroupe=styled.div`
+display:flex;
+margin-bottom:30px;
+@media (max-width:768px){
+    flex-direction:column;
+}
+`;
+//Left button component
+const LeftButton=styled.button`
+background-color:rgba(23,26,32,0.8);
+height:40px;
+width:256px;
+color:white;
+display:flex;
+justify-content:center;
+align-items:center;
+border-radius:100px;
+opacity:0.85;
+text-transform:uppercase;
+font-size:12px;
+cursor:pointer;
+margin:8px;
+
+`;
+//Right button component
+const RightButton=styled(LeftButton)`
+background-color:white;
+opacity:0.65;
+color:black;
+
+
+
+
+`;
+const DownArrow=styled.image`
+marging-top:20px;
+height:40px;
+`;
+
+const Buttons=styled.div``;
+
 export default Section
